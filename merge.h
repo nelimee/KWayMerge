eead07f68511f74955d07db8ba954fed1c32b6e0
Adrien Suau <adrien.suau@grenoble-inp.org>
2018-04-26 23:29:30 +0200

    merge now uses an external method iterative_merge

--- merge.h
+++ merge.h
@@ -26,41 +26,34 @@ using Container_t = Container<T, std::allocator<T>>;
  * @brief Efficient stable k-way merge.
  *
  *
- * This function merges k sorted STL-like containers (InternalContainer) into
- * one sorted InternalContainer.
+ * This function merges k sorted STL-like containers into one sorted container
+ * of the same type.
  *
  * The merge is stable:
- * -# Ordering of equivalent elements within the same InternalContainer is
- * preserved.
- * -# Ordering of equivalent elements from different InternalContainers follows
- * the InternalContainers ordering.
+ * -# Ordering of equivalent elements within the same container is preserved.
+ * -# Ordering of equivalent elements from different containers follows the
+ * containers ordering.
  *
  * The function is parallelised using OpenMP when the library is available.
  *
  * Overall complexity is:
  *
  *
- * @tparam T type of the objects to sort.
- * @tparam InternalContainer STL-like container used to store the objects of
- * type T. This is the type of the k lists to merge as well as the type of the
- * returned sorted list.
- * @tparam ExternalContainer STL-like container that can be iterated on with
- * iterators verifying at least the ForwardIterator requirements.
+ * @tparam ContainerOfContainers structure containing the sorted container to
+ * merge.
  * @tparam Comp Comparator used to merge the lists. Each list in @p arrays
  * should be sorted according to this comparator.
  * @param arrays k sorted lists to merge.
  * @param comp See template parameter @p Comp.
- * @return a sorted list.
+ * @return a sorted container.
  * @warning For the moment, InternalContainer should be std::vector.
  */
 template<
-		typename T,
-		template<class, class> typename InternalContainer,
-		template<class, class> typename ExternalContainer,
-		typename Comp = std::less<T>
+		typename ContainerOfContainers,
+		typename Comp = std::less<typename ContainerOfContainers::value_type::value_type>
 >
-Container_t<InternalContainer, T>
-merge_arrays(Container_t<ExternalContainer, Container_t<InternalContainer, T>> const &arrays,
+typename ContainerOfContainers::value_type
+merge_arrays(ContainerOfContainers const &arrays,
              Comp comp = Comp()) {
 
 	/* Note about complexity:
@@ -81,35 +74,36 @@ merge_arrays(Container_t<ExternalContainer, Container_t<InternalContainer, T>> c
 
 	// 1. Useful types definition
 	// Complexity: O(0) (compile-time).
-	using ExternalContainer_t = Container_t<ExternalContainer, Container_t<InternalContainer, T>>;
-	using InternalContainer_t = Container_t<InternalContainer, T>;
+	using ExternalContainer_t = ContainerOfContainers;
+	using InternalContainer_t = typename ContainerOfContainers::value_type;
+	using StoredDataType_t    = typename InternalContainer_t::value_type;
 
 	// 1.1. Check that the ExternalContainer can be iterated on with at least a ForwardIterator.
-	using ExternalContainerIterator = typename ExternalContainer<T, std::allocator<T>>::iterator;
+	using ExternalContainerIterator = typename ExternalContainer_t::iterator;
 	using ExternalContainerIteratorCategory = typename std::iterator_traits<ExternalContainerIterator>::iterator_category;
 	static_assert(std::is_same<ExternalContainerIteratorCategory, std::forward_iterator_tag>::value ||
 	              std::is_same<ExternalContainerIteratorCategory, std::bidirectional_iterator_tag>::value ||
 	              std::is_same<ExternalContainerIteratorCategory, std::random_access_iterator_tag>::value,
-	              "The ExternalContainer type should be ForwardIterable");
+	              "The type used as external container should be ForwardIterable");
 	// 1.2. For the moment, the InternalContainer can only be a std::vector.
-	static_assert(std::is_same<Container_t<InternalContainer, T>, std::vector<T> >::value,
-	              "InternalContainer should be std::vector.");
+	static_assert(std::is_same<InternalContainer_t, std::vector<StoredDataType_t> >::value,
+	              "The type used as internal container should be std::vector.");
 
 	// 2. Create the resulting structure and reserve enough space for it.
-	// Complexity: - Sequential: O(k)
+	// Complexity: - Sequential: O(k + N*TDefInit)  ==  O(N*TDefInit)
 	//             - Parallel:   O(k + N*TDefInit)  ==  O(N*TDefInit)
 
 	// 2.1. Computing the total number of elements in order to save some re-allocations.
 	// Complexity: O(k)
-	auto add_size = [](std::size_t const &previous_size, std::vector<T> const &elem) {
+	auto add_size = [](std::size_t const &previous_size, std::vector<StoredDataType_t> const &elem) {
 		return previous_size + elem.size();
 	};
 	std::size_t const number_of_elements{
 			std::accumulate(arrays.begin(), arrays.end(), static_cast<std::size_t>(0), add_size)};
 	// 2.2. Create the structure.
-	// Complexity: - Sequential: O(1)
+	// Complexity: - Sequential: O(N*TDefInit)
 	//             - Parallel:   O(N*TDefInit)
-//#ifdef _OPENMP
+
 	// Here we need to reserve enough memory because std::merge will write on it.
 	// The interface of std::vector does not allow us to use memory without
 	// initialising it so a simple "reserve" is not enough. A resize should be
@@ -129,7 +123,8 @@ merge_arrays(Container_t<ExternalContainer, Container_t<InternalContainer, T>> c
 		return result;
 	}
 	else if (number_of_lists_to_merge == 1) {
-		std::copy(arrays.front().begin(), arrays.front().end(), result.begin());
+		auto const front = *(arrays.begin());
+		std::copy(front.begin(), front.end(), result.begin());
 		return result;
 	}
 	else if (number_of_lists_to_merge == 2) {
@@ -144,48 +139,7 @@ merge_arrays(Container_t<ExternalContainer, Container_t<InternalContainer, T>> c
 	auto separators = detail::first_round_merge(arrays, result, comp);
 
 	// 6. Now result contains all the values, we just need to merge all the lists not merged in the previous step.
-	// In the separators list, we also have the begin() and end() iterators, that is why our stop condition is
-	// that the separators list should contain only 2 elements: the begin() and the end()
-	// Complexity:
-	//   - Work: O(k*M*log2(k))
-	//   - Depth: O(k*M*log2(k)/p)
-#pragma omp parallel
-	{
-#pragma omp single nowait
-		{
-			std::size_t const merging_pass_number{
-					static_cast<std::size_t>(std::ceil(std::log2(block_separator_number_after_first_round)))};
-			// O(log2(k)) iterations
-			for (std::size_t i{0}; i < merging_pass_number; ++i) {
-				auto left_iterator = separators.begin();
-				auto middle_iterator = std::next(left_iterator);
-				decltype(middle_iterator) right_iterator;
-				// We will iterate over each separators, and each time merge two blocks.
-				// Block size upper-bound: 2^(1+i)*M
-				// Upper-bound of the number of elements given to inplace_merge:
-				//   n = 2^(2+i)*M
-				// Number of merge: k/(2^(2+i))
-				while (middle_iterator != separators.end() &&
-				       (right_iterator = std::next(middle_iterator)) != separators.end()) {
-					auto left = *left_iterator;
-					auto middle = *middle_iterator;
-					auto right = *right_iterator;
-#pragma omp task firstprivate(left, middle, right) shared(result)
-					{
-						// Do the merge in-place!
-						// O(n) if there is enough memory available, else O(n*log(n))
-						std::inplace_merge(left, middle, right, comp);
-					}
-					// Remove the separator in the middle that now points at the middle of a sorted part.
-					separators.erase_after(left_iterator);
-					// And we update the iterator to merge the 2 next non-sorted blocks.
-					left_iterator = std::next(left_iterator);
-					middle_iterator = std::next(left_iterator);
-				}
-#pragma omp taskwait
-			}
-		}
-	}
+	detail::iterative_merge(result, separators, block_separator_number_after_first_round, comp);
 
 	// Return the result
 	return result;
